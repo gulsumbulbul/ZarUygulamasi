059a57ddd76fc942a8183ae55f41b308342dbe1f
Gülsüm Bülbül <148989106+gulsumbulbul@users.noreply.github.com>
2025-02-27 12:34:18 +0300

    Initial Commit

---
+++ ZarUygulamasi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZarUygulamasi
+//
+//  Created by Gülsüm Bülbül on 27.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZarUygulamasi/ZarUygulamasiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZarUygulamasiApp.swift
+//  ZarUygulamasi
+//
+//  Created by Gülsüm Bülbül on 27.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct ZarUygulamasiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
