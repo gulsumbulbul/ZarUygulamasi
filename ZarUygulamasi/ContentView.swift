e7b81448bd31e0e94e20a664598d916323c4d39c
Gülsüm Bülbül <148989106+gulsumbulbul@users.noreply.github.com>
2025-02-27 13:24:02 +0300

    proje yüklendi

--- ZarUygulamasi/ContentView.swift
+++ ZarUygulamasi/ContentView.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var zar1 = 1
+    @State private var zar2 = 1
+    @State private var skor = 2
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            HStack {
+                ZarImage(sayi: zar1)
+                ZarImage(sayi: zar2)
+            }
+            .padding()
+            Text("score:  \(skor)")
+                .font(.largeTitle)
+                .padding()
+            Button("Zarları at"){
+                randomSayi()
+                skorHesaplama()
+            }.buttonStyle(.borderedProminent).font(.largeTitle)
         }
-        .padding()
+    }
+    
+    func randomSayi() {
+        zar1 = Int.random(in: 1...6)
+        zar2 = Int.random(in: 1...6)
+    }
+    
+    func skorHesaplama(){
+        skor = zar1 + zar2
+    }
+    
+}
+
+struct ZarImage: View{
+    var sayi: Int
+    var body: some View{
+        Image(systemName: "die.face.\(sayi).fill")
+            .resizable()
+            .frame(width: 150, height: 150)
+            .padding()
     }
 }
 
